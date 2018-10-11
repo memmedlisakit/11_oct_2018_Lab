0ede059853855f122a026be82cb24c7403784db5
memmedlisakit <memmedlisakit@gmail.com>
2018-10-11 20:09:40 +0400

    lab tasks

---
+++ Tasks/Form1.Designer.cs
@@ -0,0 +1,122 @@
+﻿namespace Tasks
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnSync = new System.Windows.Forms.Button();
+            this.btnAsync = new System.Windows.Forms.Button();
+            this.dgvData = new System.Windows.Forms.DataGridView();
+            this.Column4 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column2 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column3 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvData)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnSync
+            // 
+            this.btnSync.Location = new System.Drawing.Point(645, 438);
+            this.btnSync.Name = "btnSync";
+            this.btnSync.Size = new System.Drawing.Size(75, 23);
+            this.btnSync.TabIndex = 0;
+            this.btnSync.Text = "Sync";
+            this.btnSync.UseVisualStyleBackColor = true;
+            this.btnSync.Click += new System.EventHandler(this.btn_Click);
+            // 
+            // btnAsync
+            // 
+            this.btnAsync.Location = new System.Drawing.Point(522, 438);
+            this.btnAsync.Name = "btnAsync";
+            this.btnAsync.Size = new System.Drawing.Size(75, 23);
+            this.btnAsync.TabIndex = 1;
+            this.btnAsync.Text = "Async";
+            this.btnAsync.UseVisualStyleBackColor = true;
+            this.btnAsync.Click += new System.EventHandler(this.btnAsync_Click);
+            // 
+            // dgvData
+            // 
+            this.dgvData.AllowUserToAddRows = false;
+            this.dgvData.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dgvData.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvData.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Column4,
+            this.Column1,
+            this.Column2,
+            this.Column3});
+            this.dgvData.Location = new System.Drawing.Point(12, 12);
+            this.dgvData.Name = "dgvData";
+            this.dgvData.Size = new System.Drawing.Size(708, 372);
+            this.dgvData.TabIndex = 2;
+            // 
+            // Column4
+            // 
+            this.Column4.HeaderText = "Count";
+            this.Column4.Name = "Column4";
+            // 
+            // Column1
+            // 
+            this.Column1.HeaderText = "Id";
+            this.Column1.Name = "Column1";
+            // 
+            // Column2
+            // 
+            this.Column2.HeaderText = "Name";
+            this.Column2.Name = "Column2";
+            // 
+            // Column3
+            // 
+            this.Column3.HeaderText = "Email";
+            this.Column3.Name = "Column3";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(732, 473);
+            this.Controls.Add(this.dgvData);
+            this.Controls.Add(this.btnAsync);
+            this.Controls.Add(this.btnSync);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dgvData)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnSync;
+        private System.Windows.Forms.Button btnAsync;
+        private System.Windows.Forms.DataGridView dgvData;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column4;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column2;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column3;
+    }
+}
+
---
+++ Tasks/Form1.cs
@@ -0,0 +1,131 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.Entity;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Tasks
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btn_Click(object sender, EventArgs e)
+        {
+            //Task task = new Task(() =>
+            //{
+            //    for (int i = 0; i < 3000; i++)
+            //    {
+            //        this.lblResponse.Text = i.ToString();
+            //    }
+            //});
+
+            //task.Start();
+
+            //new Task(() =>
+            //{
+            //    for (int i = 0; i < 3000; i++)
+            //    {
+            //        this.lbl2.Text = i.ToString();
+            //    }
+            //}).Start();
+
+
+            //task.ContinueWith((_task) =>
+            //{
+            //    MessageBox.Show("Finished " + _task.Id);
+            //});
+
+            using(SaleEntities db =new SaleEntities())
+            {
+                dgvData.Rows.Clear();
+                List<Cutomer> customers = db.Cutomers.ToList(); 
+                int rowIndex = 0;
+                foreach (Cutomer item in customers)
+                {
+                    dgvData.Rows.Add();
+                    dgvData.Rows[rowIndex].Cells[0].Value = (rowIndex + 1);
+                    dgvData.Rows[rowIndex].Cells[1].Value = item.Id;
+                    dgvData.Rows[rowIndex].Cells[2].Value = item.Name;
+                    dgvData.Rows[rowIndex].Cells[3].Value = item.Email;
+                    rowIndex++;
+                }
+            }
+            MessageBox.Show("Complited Succesfuly"); 
+        }
+
+        private async void btnAsync_Click(object sender, EventArgs e)
+        {
+
+            //Task task = new Task(()=> 
+            //{
+            //   using(SaleEntities db =new SaleEntities())
+            //    {
+            //        dgvData.Rows.Clear();
+            //        List<Cutomer> customers = db.Cutomers.ToList();
+            //        int rowIndex = 0;
+            //        foreach (Cutomer item in customers)
+            //        {
+            //            dgvData.Rows.Add();
+            //            dgvData.Rows[rowIndex].Cells[0].Value = (rowIndex + 1);
+            //            dgvData.Rows[rowIndex].Cells[1].Value = item.Id;
+            //            dgvData.Rows[rowIndex].Cells[2].Value = item.Name;
+            //            dgvData.Rows[rowIndex].Cells[3].Value = item.Email;
+            //            rowIndex++;
+            //        }
+            //    }
+
+            //});
+
+            //task.Start();
+
+            //task.ContinueWith((x) =>
+            //{
+            //    MessageBox.Show("Complited Succesfuly");
+            //});
+
+
+
+            await ReadDataAsync();
+
+
+        }
+
+        public async Task ReadDataAsync()
+        {  
+           await Task.Run(() =>
+            {
+                dgvData.Rows.Clear();
+                List<Cutomer> customers = GetAllDataAsync().Result.ToList();
+                int rowIndex = 0;
+                foreach (Cutomer item in customers)
+                {
+                    dgvData.Rows.Add();
+                    dgvData.Rows[rowIndex].Cells[0].Value = (rowIndex + 1);
+                    dgvData.Rows[rowIndex].Cells[1].Value = item.Id;
+                    dgvData.Rows[rowIndex].Cells[2].Value = item.Name;
+                    dgvData.Rows[rowIndex].Cells[3].Value = item.Email;
+                    rowIndex++;
+                }
+            });
+
+        }
+
+
+        public async Task<IEnumerable<Cutomer>> GetAllDataAsync()
+        {
+            using (SaleEntities db = new SaleEntities())
+            {
+                return await db.Cutomers.ToListAsync();
+            }
+        }
+    }
+}
